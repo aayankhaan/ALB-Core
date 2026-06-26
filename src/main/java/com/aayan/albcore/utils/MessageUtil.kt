941def8bb9a4effdccc54f6d07140464ffacbc8d
aayan khan <133193801+aayankhaan@users.noreply.github.com>
2026-06-26 13:35:46 -0500

    Added PAPI support to MessageUtil

--- src/main/java/com/aayan/albcore/utils/MessageUtil.kt
+++ src/main/java/com/aayan/albcore/utils/MessageUtil.kt
@@ -1,5 +1,6 @@
 package com.aayan.albcore.utils
 
+import com.aayan.albcore.hooks.PAPIHook
 import net.kyori.adventure.title.Title
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
@@ -12,8 +13,12 @@ object MessageUtil {
         sender.sendMessage(ColorUtil.parse(text))
     }
 
+    fun sendWithPAPI(player: Player, text: String) {
+        player.sendMessage(ColorUtil.parse(PAPIHook.parse(player,text)))
+    }
+
     fun sendTitle(player: Player, title: String, subtitle: String, fadeIn: Long = 500, Stay: Long = 3000, fadeOut: Long = 500) {
-        player.showTitle(Title.title(ColorUtil.parse(title), ColorUtil.parse(subtitle), Title.Times.times(Duration.ofMillis(fadeIn),Duration.ofMillis(Stay),Duration.ofMillis(fadeOut))))
+        player.showTitle(Title.title(ColorUtil.parse(PAPIHook.parse(player,title)), ColorUtil.parse(subtitle), Title.Times.times(Duration.ofMillis(fadeIn),Duration.ofMillis(Stay),Duration.ofMillis(fadeOut))))
     }
 
     fun broadcast(text: String) {
